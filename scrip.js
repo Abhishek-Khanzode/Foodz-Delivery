e6ab2ed18b147cb2a8031f7cd0848b948adf40b0
Abhishek-Khanzode <abhi.geek2004@gmail.com>
2024-03-01 20:03:04 +0530

    Made changes to the homepage

--- scrip.js
+++ scrip.js
@@ -11,22 +11,22 @@ menuIcon.onclick = () => {
 
 
 var arr = [
-    {name: "roses", image: "./assets/images/bg.jpg"},
-    {name: "car", image: "./assets/images/bg.jpg"},
-    {name: "boike", image: "img2../assets/images/bg.jpg"},
-    {name: "dee", image: "./assets/images/bg.jpg"},
-    {name: "dee", image: "./assets/images/bg.jpg"},
-    {name: "dee", image: "./assets/images/bg.jpg"},
-    {name: "dee", image: "./assets/images/bg.jpg"},
-    {name: "dee", image: "./assets/images/bg.jpg"},
-    {name: "dee", image: "./assets/images/bg.jpg"},
-    {name: "dee", image: "./assets/images/bg.jpg"},
-    {name: "dee", image: "./assets/images/bg.jpg"},
-    {name: "dee", image: "./assets/images/bg.jpg"},
-    {name: "dee", image: "./assets/images/bg.jpg"},
-    {name: "dee", image: "./assets/images/bg.jpg"},
-    {name: "dee", image: "./assets/images/bg.jpg"},
-    {name: "dee", image: "./assets/images/bg.jpg"}
+    {name: "car", image: "./assets/images/bg.webp"},
+    {name: "ros", image: "./assets/images/bg.webp"},
+    {name: "boi", image: "./assets/images/bg.webp"},
+    {name: "dee", image: "./assets/images/bg.webp"},
+    {name: "dee", image: "./assets/images/bg.webp"},
+    {name: "dee", image: "./assets/images/bg.webp"},
+    {name: "dee", image: "./assets/images/bg.webp"},
+    {name: "dee", image: "./assets/images/bg.webp"},
+    {name: "dee", image: "./assets/images/bg.webp"},
+    {name: "dee", image: "./assets/images/bg.webp"},
+    {name: "dee", image: "./assets/images/bg.webp"},
+    {name: "dee", image: "./assets/images/bg.webp"},
+    {name: "dee", image: "./assets/images/bg.webp"},
+    {name: "dee", image: "./assets/images/bg.webp"},
+    {name: "dee", image: "./assets/images/bg.webp"},
+    {name: "dee", image: "./assets/images/bg.webp"}
     // {name: "dee", image: "./assets/images/bg.jpg"}
 ]
 
@@ -36,7 +36,7 @@ function showTheCards(){
     var clutter = "";
     arr.forEach(function(obj){
         clutter += `<div class="card">
-        <img src="/assets/images/bg.jpg" alt="">
+        <img src="/assets/images/bg.webp" alt="">
         <h2>halwa</h2>
         <p>20$</p>
     </div>`;
